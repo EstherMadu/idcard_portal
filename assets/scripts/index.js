8be2e465cdb4957ffb82d50a41bec911571ce0ea
esthermadu <prityexsta@gmail.com>
2022-09-22 00:10:21 +0100

    worked on the second page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,23 +1,33 @@
 const form = document.querySelector("form"),
   nextBtn = form.querySelector(".nextBtn"),
-  nextBtnSecond = form.querySelector(".nextBtnSecond"),
+  // nextBtnSecond = form.querySelector(".nextBtnSecond"),
   backBtn = form.querySelector(".backBtn"),
   allInput = form.querySelectorAll(".first input"),
-  allInputSecond = form.querySelectorAll(".second input");
-
-// const element = document.getElementById("form-1");
-// const formId = element.getAttribute("data-id");
-// console.log(formId);
+  allInputSecond = form.querySelectorAll(".second input"),
+  secondNext = form.querySelector(".nextBtn2");
+const formId = document.getElementById("form-1");
+const nextForm = document.getElementById("form-2");
+const thirdForm = document.getElementById("form-3");
 
 nextBtn.addEventListener("click", function () {
-  const formId = document.getElementById("form-1");
-  const nextForm = document.getElementById("form-2");
   allInput.forEach((input) => {
-    if (input.value != "") {
+    if (input.value == "") {
       formId.classList.remove("active");
       nextForm.classList.add("active");
     }
   });
 });
 
-backBtn.addEventListener("click", () => form.classList.remove("active"));
+backBtn.addEventListener("click", () => {
+  nextForm.classList.remove("active");
+  formId.classList.add("active");
+});
+
+secondNext.addEventListener("click", () => {
+  allInputSecond.forEach((input) => {
+    if (input.value == "") {
+      nextForm.classList.remove("active");
+      thirdForm.classList.add("active");
+    }
+  });
+});
