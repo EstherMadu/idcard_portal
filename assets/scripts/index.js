7fe6228034879871edbaa85e50bf6f6c4bc3fba1
esthermadu <prityexsta@gmail.com>
2022-09-21 23:36:26 +0100

    fixed issue with next button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,26 +5,19 @@ const form = document.querySelector("form"),
   allInput = form.querySelectorAll(".first input"),
   allInputSecond = form.querySelectorAll(".second input");
 
-nextBtn.addEventListener("click", () => {
+// const element = document.getElementById("form-1");
+// const formId = element.getAttribute("data-id");
+// console.log(formId);
+
+nextBtn.addEventListener("click", function () {
+  const formId = document.getElementById("form-1");
+  const nextForm = document.getElementById("form-2");
   allInput.forEach((input) => {
     if (input.value != "") {
-      form.classList.add("secActive");
-    } else {
-      form.classList.remove("secActive");
-      //alert("input is not empty");
+      formId.classList.remove("active");
+      nextForm.classList.add("active");
     }
   });
 });
 
-nextBtnSecond.addEventListener("click", () => {
-  allInputSecond.forEach((input) => {
-    if (input.value != "") {
-      form.classList.add("secActive");
-    } else {
-      form.classList.remove("secActive");
-      //alert("input is not empty");
-    }
-  });
-});
-
-backBtn.addEventListener("click", () => form.classList.remove("secActive"));
+backBtn.addEventListener("click", () => form.classList.remove("active"));
